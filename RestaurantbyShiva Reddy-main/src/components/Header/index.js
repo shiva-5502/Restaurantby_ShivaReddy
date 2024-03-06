c884c1ed41b5ad10f96b3ee033f5cf17233a2c64
B-ShivaReddy <shivareddybandi25@gmail.com>
2024-03-06 23:35:50 +0530

    Update My Restaurant file's

---
+++ RestaurantbyShiva Reddy-main/src/components/Header/index.js
@@ -0,0 +1,28 @@
+import './index.css'
+
+const Header = ({cartItems}) => {
+  const getCartItemsCount = () =>
+    cartItems.reduce((acc, item) => acc + item.quantity, 0)
+
+  const renderCartIcon = () => (
+    <div className="cart-icon-link">
+      <div className="cart-count-badge d-flex justify-content-center align-items-center">
+        <p className="m-0 cart-count">{getCartItemsCount()}</p>
+      </div>
+    </div>
+  )
+
+  return (
+    <header className="p-4 d-flex flex-row align-items-center nav-header">
+      <h1 className="m-0 logo-heading">UNI Resto Cafe</h1>
+      <div className="d-flex flex-row align-items-center ms-auto">
+        <p className="mt-0 mb-0 me-2 d-none d-sm-block my-orders-text">
+          My Orders
+        </p>
+        {renderCartIcon()}
+      </div>
+    </header>
+  )
+}
+
+export default Header
